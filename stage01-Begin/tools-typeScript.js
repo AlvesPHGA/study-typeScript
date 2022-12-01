818eb5089ba50dac8121dbcbeb3e72f90fbb8308
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-12-01 14:13:05 -0300

    satege02-basic: start

---
+++ stage01-Begin/tools-typeScript.js
@@ -0,0 +1,5 @@
+"use strict";
+function sum(a, b, c) {
+    return a + b - c;
+}
+console.log(sum(30, 20.69, 30));
---
+++ stage01-Begin/tools-typeScript.ts
@@ -0,0 +1,5 @@
+function sum(a: number, b: number, c: number) {
+   return a + b - c;
+}
+
+console.log(sum(30, 20.69, 30));
