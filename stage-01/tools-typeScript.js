565d9b5bba48129bbed1ff1142c2c5f82a451c7b
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-12-01 11:42:21 -0300

    rename stage-01 to stage01-Begin

--- stage-01/tools-typeScript.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function sum(a, b, c) {
-    return a + b - c;
-}
-console.log(sum(30, 20.69, 30));
--- stage-01/tools-typeScript.ts
+++
@@ -1,5 +0,0 @@
-function sum(a: number, b: number, c: number) {
-   return a + b - c;
-}
-
-console.log(sum(30, 20.69, 30));
