87ba027254e8923301db676faba400ab755e473e
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-12-09 11:19:48 -0300

    add three examples in instaceof

--- stage03-ObjectInterfaces/instanceof.js
+++ stage03-ObjectInterfaces/instanceof.js
@@ -1,5 +1,18 @@
 "use strict";
 // example 01
+class Product {
+    name;
+    price;
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    priceReal() {
+        return `R$ ${this.price}`;
+    }
+}
+const feed = new Product('Laptop', 2000);
+// example 02 - whit instaceof
 class Book {
     name;
     constructor(name) {
@@ -7,24 +20,49 @@ class Book {
     }
 }
 class Game {
-    player;
-    constructor(player) {
-        this.player = player;
-    }
-    company() {
-        const nameCompany = 'Giants';
-        return nameCompany;
+    players;
+    constructor(players) {
+        this.players = players;
     }
 }
 function searchProduct(search) {
-    if (search === 'GTA') {
-        return new Game(1);
-    }
-    else if (search === 'Jogos Vorazes') {
-        return new Book('Jonnas Day');
-    }
+    if (search === 'O Hobbit')
+        return new Book('J. R. R. Tolkien');
+    if (search === 'GTA')
+        return new Game(2);
     return null;
 }
-const product = searchProduct('GTA');
-if (product instanceof Game)
-    console.log(product.player);
+const product01 = searchProduct('GTA');
+// if (product instanceof Game) console.log(product.players);
+// if (product instanceof Book) console.log(product.name);
+// Exmaple 03 - with extends
+class Product02 {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Laptop extends Product02 {
+    price;
+    constructor(name, price) {
+        super(name);
+        this.price = price;
+    }
+}
+class Movel extends Product02 {
+    lugar;
+    constructor(name, lugar) {
+        super(name);
+        this.lugar = lugar;
+    }
+}
+function searchProduct02(search) {
+    if (search === 'Dell')
+        return new Laptop('Inspiron', 2000.99);
+    if (search === 'Sofa')
+        return new Movel('Sofa Retratel', 4);
+    return null;
+}
+const product02 = searchProduct02('Dell');
+if (product02 instanceof Laptop)
+    console.log(product02.price);
--- stage03-ObjectInterfaces/instanceof.ts
+++ stage03-ObjectInterfaces/instanceof.ts
@@ -1,49 +1,76 @@
 // example 01
+class Product {
+   name: string;
+   price: number;
+
+   constructor(name: string, price: number) {
+      this.name = name;
+      this.price = price;
+   }
+
+   priceReal() {
+      return `R$ ${this.price}`;
+   }
+}
+
+const feed = new Product('Laptop', 2000);
+
+// example 02 - whit instaceof
 class Book {
    name: string;
-
    constructor(name: string) {
       this.name = name;
    }
-
-   // author() {
-   //    const nameAuthor = 'Richard Goes';
-   //    return nameAuthor;
-   // }
-
-   // price() {
-   //    const price = 'R$ 400';
-   //    return price;
-   // }
 }
 
 class Game {
-   player: number;
-
-   constructor(player: number) {
-      this.player = player;
+   players: number;
+   constructor(players: number) {
+      this.players = players;
    }
-
-   company() {
-      const nameCompany = 'Giants';
-      return nameCompany;
-   }
-
-   // price() {
-   //    const price = 'R$ 250';
-   //    return price;
-   // }
 }
 
 function searchProduct(search: string) {
-   if (search === 'GTA') {
-      return new Game(1);
-   } else if (search === 'Jogos Vorazes') {
-      return new Book('Jonnas Day');
-   }
+   if (search === 'O Hobbit') return new Book('J. R. R. Tolkien');
+   if (search === 'GTA') return new Game(2);
    return null;
 }
 
-const product = searchProduct('GTA');
+const product01 = searchProduct('GTA');
 
-if (product instanceof Game) console.log(product.player);
+// if (product instanceof Game) console.log(product.players);
+// if (product instanceof Book) console.log(product.name);
+
+// Exmaple 03 - with extends
+class Product02 {
+   name: string;
+   constructor(name: string) {
+      this.name = name;
+   }
+}
+
+class Laptop extends Product02 {
+   price: number;
+   constructor(name: string, price: number) {
+      super(name);
+      this.price = price;
+   }
+}
+
+class Movel extends Product02 {
+   lugar: number;
+   constructor(name: string, lugar: number) {
+      super(name);
+      this.lugar = lugar;
+   }
+}
+
+function searchProduct02(search: string) {
+   if (search === 'Dell') return new Laptop('Inspiron', 2000.99);
+   if (search === 'Sofa') return new Movel('Sofa Retratel', 4);
+   return null;
+}
+
+const product02 = searchProduct02('Dell');
+
+if (product02 instanceof Laptop) console.log(product02.price);
