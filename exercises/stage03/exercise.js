d95c976515399f755c89c4c133c4e45e90a4b47b
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-12-29 12:02:33 -0300

    add a new doc in study instanceof and interfaces dom

---
+++ exercises/stage03/exercise.js
@@ -0,0 +1,20 @@
+"use strict";
+// Instaceof
+const origamidLink = document.getElementById('origamid');
+if (origamidLink instanceof HTMLAnchorElement) {
+    origamidLink.href = origamidLink.href.replace('http://', 'https://');
+}
+// Interfaces do DOM
+const components = document.querySelectorAll('.link');
+function changeColorFont(element) {
+    if (element instanceof HTMLAnchorElement)
+        element.style.color = 'red';
+    if (element instanceof HTMLButtonElement) {
+        element.style.borderColor = 'blue';
+        element.style.color = 'blue';
+    }
+}
+components.forEach((component) => {
+    if (component instanceof HTMLElement)
+        changeColorFont(component);
+});
